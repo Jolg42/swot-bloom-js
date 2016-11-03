be866c03c1984380101906f848e1de40e4eb8b05
Coward <cowardj@ELSCAMM-156213.science.regn.net>
2016-11-03 16:46:59 +0000

    Rewritten to use a bloom filter for checcking No longer returns name of insitituion from .check method Some linting changes

--- test/swot.js
+++ test/swot.js
@@ -1,37 +1,41 @@
 var expect = require('chai').expect;
 var should = require('chai').should();
 
-var assert = require("assert");
+var assert = require('assert');
 
-describe('Swot', function(){
+describe('Swot', function () {
   this.timeout(5000);
-  describe('has check function', function(){
-    it('should return true', function(){
-      var swot = require("../lib/swot.js")();
+  
+  describe('has check function', function () {
+    it('should return true', function () {
+      var swot = require('../lib/swot.js')();
       assert.equal(true, (swot.check != undefined));
     });
   });
-  describe('test @hdm-stuttgart.de', function(){
-    it('should return name of university', function(done){
-      var swot = require("../lib/swot.js")(function(){
-      		assert.equal("Fachhochschule Stuttgart, Hochschule der Medien\n",swot.check("mv037@hdm-stuttgart.de"));
-      		done();
+
+  describe('test @hdm-stuttgart.de', function () {
+    it('should return true', function (done) {
+      var swot = require('../lib/swot.js')(function () {
+        assert.equal(true, swot.check('mv037@hdm-stuttgart.de'));
+        done();
       });
     });
   });
-  describe('test @test.de', function(){
-    it('should return false', function(done){
-      var swot = require("../lib/swot.js")(function(){
-      		assert.equal(false,swot.check("mv037@test.de"));
-      		done();
+
+  describe('test @test.de', function () {
+    it('should return false', function (done) {
+      var swot = require('../lib/swot.js')(function () {
+        assert.equal(false, swot.check('mv037@test.de'));
+        done();
       });
     });
   });
-  describe('test @si.edu (blacklisted)', function(){
-    it('should return false', function(done){
-      var swot = require("../lib/swot.js")(function(){
-      		assert.equal(false,swot.check("mv037@si.edu"));
-      		done();
+
+  describe('test @si.edu (blacklisted)', function () {
+    it('should return false', function (done) {
+      var swot = require('../lib/swot.js')(function () {
+        assert.equal(false, swot.check('mv037@si.edu'));
+        done();
       });
     });
   });
